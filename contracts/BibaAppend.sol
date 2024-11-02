5ae7d4f464ea53ad54f62254c680c2f12b147592
Janosia <janosia.banerjee03@gmail.com>
2024-11-02 19:26:39 +0530

    changed visibility

--- contracts/BibaAppend.sol
+++ contracts/BibaAppend.sol
@@ -4,47 +4,60 @@ pragma solidity ^0.8.17;
 import "./AssignRole.sol";
 import "./Case.sol";
 
-///@title allws additional information/supporting material to be added to an existing evidence
-contract BibaAppend{
-   
-   AssignRole public  user;
-   Case public scase;
-   mapping (bytes32 => mapping(uint => bytes32)) private AppendedEvidence;  // keep track of which evidence was appended to, but an evidence can be appended to multiple times
-   mapping (bytes32 =>uint) private TrackerMapping; // keep track of how many times and evidence was appended to 
-   
-   //Evidence public evidence;
-   event AppendAllowed(string, address, bytes32, bytes32); // event for blockchain
-   
-   constructor( address _assignRole, address _case){
-    user = AssignRole(_assignRole);
-    scase = Case(_case);}
+///@title allows additional information/supporting material to be added to an existing evidence
+contract BibaAppend {
+    AssignRole private user;
+    Case private scase;
+    mapping(bytes32 => mapping(uint => bytes32)) private AppendedEvidence; // keep track of which evidence was appended to, but an evidence can be appended to multiple times
+    mapping(bytes32 => uint) private TrackerMapping; // keep track of how many times and evidence was appended to
 
-   function returnTimes(bytes32 key) private view returns(uint){  // return number of times an evidence was appended to
-      return TrackerMapping[key];
-   }
+    //Evidence public evidence;
+    event AppendAllowed(string, address, bytes32, bytes32); // event for blockchain
 
-   function append_allowed(bytes32 key, address ad_user, bytes32 key_of_new, uint case_num) private returns (bool){
-    require(scase.is_authorized(ad_user, case_num) == true, "Cannot interact with this evidence");
-    uint L = scase.return_level(case_num, key);
-    uint R = user.returnRole(ad_user);
-    bool val = (R>=L);
-    
-    require(val == true, "User is not authorized to append to evidence");
-    
-    uint times = returnTimes(key); // find number of times the original evidence was appended to
-    uint ntime = times+1;
-    TrackerMapping[key] = ntime; // update times the original evidenece was appended to
-    
-    // Take input from user; this is placeholder
+    constructor(address _assignRole, address _case) {
+        user = AssignRole(_assignRole);
+        scase = Case(_case);
+    }
 
-    bytes32 newhash  = keccak256(abi.encodePacked(key,key_of_new)); // calculate new hash
-    string memory str = "Hell";
-    scase.register_evi(case_num, str, msg.sender); // create a new evidence with new founf evidence related to og evidence
-    
-    AppendedEvidence[key][ntime] = newhash; // finally add the appended evidence in the nested mapping 
-    emit AppendAllowed("User has appended to evidence", ad_user, key, newhash);
-    
-    return true;
-   }
+    function returnTimes(bytes32 key) private view returns (uint) {
+        // return number of times an evidence was appended to
+        return TrackerMapping[key];
+    }
 
+    function append_allowed(
+        bytes32 key,
+        address ad_user,
+        bytes32 key_of_new,
+        uint case_num
+    ) private returns (bool) {
+        require(
+            scase.is_authorized(ad_user, case_num) == true,
+            "Cannot interact with this evidence"
+        );
+        uint L = scase.return_level(case_num, key);
+        uint R = user.returnRole(ad_user);
+        bool val = (R >= L);
+
+        require(val == true, "User is not authorized to append to evidence");
+
+        uint times = returnTimes(key); // find number of times the original evidence was appended to
+        uint ntime = times + 1;
+        TrackerMapping[key] = ntime; // update times the original evidenece was appended to
+
+        // Take input from user; this is placeholder
+
+        bytes32 newhash = keccak256(abi.encodePacked(key, key_of_new)); // calculate new hash
+        string memory str = "Hell";
+        scase.register_evi(case_num, str, msg.sender); // create a new evidence with new founf evidence related to og evidence
+
+        AppendedEvidence[key][ntime] = newhash; // finally add the appended evidence in the nested mapping
+        emit AppendAllowed(
+            "User has appended to evidence",
+            ad_user,
+            key,
+            newhash
+        );
+
+        return true;
+    }
 }
