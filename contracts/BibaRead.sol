a0fc2c46759088556fb9491c33def169dd9cbe25
Janosia <janosia.banerjee03@gmail.com>
2024-11-02 19:28:45 +0530

    changed visibility in BibaRead

--- contracts/BibaRead.sol
+++ contracts/BibaRead.sol
@@ -4,29 +4,40 @@ pragma solidity ^0.8.17;
 import "./AssignRole.sol";
 import "./Case.sol";
 
-contract BibaRead{
-   AssignRole public  user; 
-   Case public scase;
+///@title allows user to read an existing evidence
 
-   mapping (address => bytes32) ReadEvidence;  // keep track of which user read which evidence, one user can read multiple evidences so like is this needed
+contract BibaRead {
+    AssignRole private user;
+    Case private scase;
 
-   event ReadAllowed(string, address, bytes32); // event for blockchain
-   
-   constructor(address _assignRole, address _case){
-    user = AssignRole(_assignRole);
-    scase = Case(_case);
-   }
+    mapping(address => bytes32) private ReadEvidence; // keep track of which user read which evidence, one user can read multiple evidences so like is this needed
 
-   function read_allowed(bytes32 key, address ad_user, uint case_num) public payable returns (bool){
-    require(scase.is_authorized(ad_user, case_num) == true, "Cannot interact with this evidence"); // checking DAC
-    require(scase.is_level_assigned(case_num, key)== true, "Evidnece cannot be interacted with , it has not been assigned a level"); // unassigned levels 
-    uint L = scase.return_level(case_num, key);
-    uint R = user.returnRole(ad_user);
+    event ReadAllowed(string, address, bytes32); // event for blockchain
 
-    bool val = (R<=L);
-    require(val == true, "User is not authorized to read evidence");
-    emit ReadAllowed("User has read evidence", ad_user, key);
-    return true;
-   }
+    constructor(address _assignRole, address _case) {
+        user = AssignRole(_assignRole);
+        scase = Case(_case);
+    }
 
+    function read_allowed(
+        bytes32 key,
+        address ad_user,
+        uint case_num
+    ) private returns (bool) {
+        require(
+            scase.is_authorized(ad_user, case_num) == true,
+            "Cannot interact with this evidence"
+        ); // checking DAC
+        require(
+            scase.is_level_assigned(case_num, key) == true,
+            "Evidnece cannot be interacted with , it has not been assigned a level"
+        ); // unassigned levels
+        uint L = scase.return_level(case_num, key);
+        uint R = user.returnRole(ad_user);
+
+        bool val = (R <= L);
+        require(val == true, "User is not authorized to read evidence");
+        emit ReadAllowed("User has read evidence", ad_user, key);
+        return true;
+    }
 }
