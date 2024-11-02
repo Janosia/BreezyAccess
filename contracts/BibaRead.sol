2a816c617ad11ff33a03b5340b46a7878929975d
Janosia <janosia.banerjee03@gmail.com>
2024-11-03 01:11:18 +0530

    Used inheritance instead of instances in BibaRead.sol

--- contracts/BibaRead.sol
+++ contracts/BibaRead.sol
@@ -6,18 +6,10 @@ import "./Case.sol";
 
 ///@title allows user to read an existing evidence
 
-contract BibaRead {
-    AssignRole private user;
-    Case private scase;
-
+contract BibaRead is AssignRole, Case {
     mapping(address => bytes32) private ReadEvidence; // keep track of which user read which evidence, one user can read multiple evidences so like is this needed
 
     event ReadAllowed(string, address, bytes32); // event for blockchain
-
-    constructor(address _assignRole, address _case) {
-        user = AssignRole(_assignRole);
-        scase = Case(_case);
-    }
 
     function read_allowed(
         bytes32 key,
@@ -25,15 +17,15 @@ contract BibaRead {
         uint case_num
     ) private returns (bool) {
         require(
-            scase.is_authorized(ad_user, case_num) == true,
+            is_authorized(ad_user, case_num) == true,
             "Cannot interact with this evidence"
         ); // checking DAC
         require(
-            scase.is_level_assigned(case_num, key) == true,
+            is_level_assigned(case_num, key) == true,
             "Evidnece cannot be interacted with , it has not been assigned a level"
         ); // unassigned levels
-        uint L = scase.return_level(case_num, key);
-        uint R = user.returnRole(ad_user);
+        uint L = return_level(case_num, key);
+        uint R = returnRole(ad_user);
 
         bool val = (R <= L);
         require(val == true, "User is not authorized to read evidence");
