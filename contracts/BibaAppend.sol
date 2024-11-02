d7df84c4a054e0b28848105b44ac88413584aee2
Janosia <janosia.banerjee03@gmail.com>
2024-11-03 01:14:43 +0530

    removed instances and inheritance used

--- contracts/BibaAppend.sol
+++ contracts/BibaAppend.sol
@@ -5,19 +5,12 @@ import "./AssignRole.sol";
 import "./Case.sol";
 
 ///@title allows additional information/supporting material to be added to an existing evidence
-contract BibaAppend {
-    AssignRole private user;
-    Case private scase;
+contract BibaAppend is AssignRole, Case {
     mapping(bytes32 => mapping(uint => bytes32)) private AppendedEvidence; // keep track of which evidence was appended to, but an evidence can be appended to multiple times
     mapping(bytes32 => uint) private TrackerMapping; // keep track of how many times and evidence was appended to
 
     //Evidence public evidence;
     event AppendAllowed(string, address, bytes32, bytes32); // event for blockchain
-
-    constructor(address _assignRole, address _case) {
-        user = AssignRole(_assignRole);
-        scase = Case(_case);
-    }
 
     function returnTimes(bytes32 key) private view returns (uint) {
         // return number of times an evidence was appended to
@@ -31,11 +24,11 @@ contract BibaAppend {
         uint case_num
     ) private returns (bool) {
         require(
-            scase.is_authorized(ad_user, case_num) == true,
+            is_authorized(ad_user, case_num) == true,
             "Cannot interact with this evidence"
         );
-        uint L = scase.return_level(case_num, key);
-        uint R = user.returnRole(ad_user);
+        uint L = return_level(case_num, key);
+        uint R = returnRole(ad_user);
         bool val = (R >= L);
 
         require(val == true, "User is not authorized to append to evidence");
@@ -48,7 +41,7 @@ contract BibaAppend {
 
         bytes32 newhash = keccak256(abi.encodePacked(key, key_of_new)); // calculate new hash
         string memory str = "Hell";
-        scase.register_evi(case_num, str, msg.sender); // create a new evidence with new founf evidence related to og evidence
+        register_evi(case_num, str, msg.sender); // create a new evidence with new founf evidence related to og evidence
 
         AppendedEvidence[key][ntime] = newhash; // finally add the appended evidence in the nested mapping
         emit AppendAllowed(
