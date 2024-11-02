429a26887ce73fba7233ce99aa2512997be877c7
Janosia <janosia.banerjee03@gmail.com>
2024-11-02 19:41:19 +0530

    removed instances in Register.sol

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -3,14 +3,10 @@ pragma solidity ^0.8.17;
 
 import "./AssignRole.sol";
 
-contract Register{
-    AssignRole public roleContract;
+contract Register is AssignRole{
     
-    constructor (address _roleContract){
-        roleContract = AssignRole(_roleContract);
-    }
-    function register_user(string memory designation) public {  
-        roleContract.setRole(designation, msg.sender);
+    function register_user(string calldata designation) public {  
+        setRole(designation, msg.sender);
     }
     Register public register;
 }
