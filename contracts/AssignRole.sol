7f320796d4b5e748b6c1a5f5f02397e4bd6179b7
Janosia <janosia.banerjee03@gmail.com>
2024-11-02 19:17:56 +0530

    changed variable type in AssignRole.sol

--- contracts/AssignRole.sol
+++ contracts/AssignRole.sol
@@ -1,44 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
- 
 
-contract AssignRole{
+contract AssignRole {
     mapping(string => uint) private Levels;
-    mapping(address => uint) public Roles; // each address will have a IG level storing is like address : Integrity Level 1/2
-    address[] private RegisteredUsers;  // dynamic array of all registered users, to check whether they exists or not
-    
+    mapping(address => uint) private Roles; // each address will have a IG level storing is like address : Integrity Level 1/2
+    address[] private RegisteredUsers; // dynamic array of all registered users, to check whether they exists or not
+
     event RegistrationDone(string, address, uint);
-    
-    constructor(){
+
+    constructor() {
         Levels["Head Investigator"] = 1;
         Levels["Lead Investigator"] = 4;
         Levels["Team Investigator"] = 7;
     }
 
-    function getLevel(string memory designation) public payable returns(uint){
-       return (Levels[designation]);
+    function getLevel(
+        string calldata designation
+    ) public payable returns (uint) {
+        return (Levels[designation]);
     }
 
-    function DoesExists (address user) public view returns(bool){
-        for(uint i =0; i < RegisteredUsers.length; i++){
-            if(RegisteredUsers[i] == user){
+    function DoesExists(address user) public view returns (bool) {
+        for (uint i = 0; i < RegisteredUsers.length; i++) {
+            if (RegisteredUsers[i] == user) {
                 return true;
             }
         }
         return false;
     }
 
-    function setRole(string memory designation, address user) public payable {
-        
-        require(DoesExists(user) == false, "User Already Registered" );
-        require(getLevel(designation)> 0, "Invalid designation");
+    function setRole(string calldata designation, address user) public payable {
+        require(DoesExists(user) == false, "User Already Registered");
+        require(getLevel(designation) > 0, "Invalid designation");
         Roles[user] = getLevel(designation);
         RegisteredUsers.push(user);
-        emit RegistrationDone(" User Registration Completed",user, Roles[user]);
-        
+        emit RegistrationDone(
+            " User Registration Completed",
+            user,
+            Roles[user]
+        );
     }
-    function returnRole(address user) public view returns(uint){
-        require(DoesExists(user) == true , "User is not registered");
+    function returnRole(address user) public view returns (uint) {
+        require(DoesExists(user) == true, "User is not registered");
         return Roles[user];
     }
 }
